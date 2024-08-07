71f575fcbb74074170ead695403c2f7a6d72715f
maximk247 <kiyaniytsamaxim@gmail.com>
2024-08-07 22:51:58 +0700

    Добавление husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
- 
-
-  return (
-    <div>
-      
-    </div>
-    
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
